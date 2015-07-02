a30036f9d319e607170f54f0d74924c96d304632
root <root@archer>
2015-07-02 01:20:40 -0500

    file hierarchy

---
+++ toads/emitter.c
@@ -0,0 +1,17 @@
+/*
+Hello, I'm the EMITTER!
+I take PARSER's byte code and translate it directly into the proper hex-values for each instruction.
+My current architecture is the Z80, as adapted for the GameBoy Color.
+My output will be a fixed-size ROM file, based on header information.
+*/
+#include <stdio.h>
+char optimize();
+
+int main() {
+return 0;
+}
+
+optimize (c) {
+	// I don't have any optimizations to perform yet :'(
+	return c;
+}
